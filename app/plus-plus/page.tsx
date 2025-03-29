351f07a4d05b4b441649b3e581872a35cd27dee3
KC Lin <kcl@thefuturecraft.com>
2025-03-29 15:25:08 +0800

    fix suspend

--- app/plus-plus/page.tsx
+++ app/plus-plus/page.tsx
@@ -1,19 +1,31 @@
 "use client"
 import Counter from "./Counter"
 import { useSearchParams } from "next/navigation"
+import { Suspense } from 'react'
 
 const PlusPlus = () => {
     const searchParams = useSearchParams()
 
     const user = searchParams.get("user")
 
+
+
     return(
         <main
             className="flex flex-col"
         >
-            <h1>{user} Plus Plus</h1>
-            <Counter />
+            <Suspense fallback={<SuspendFallback/>}>
+                <h1>{user} Plus Plus</h1>
+                <Counter />
+            </Suspense>
+            
         </main>
+    )
+}
+
+const SuspendFallback = () => {
+    return(
+        <p>Loading...</p>
     )
 }
 
