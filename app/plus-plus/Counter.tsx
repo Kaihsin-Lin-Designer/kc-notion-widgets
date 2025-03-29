7dafd28b704b27c8c27c218845136a56357fb94b
KC Lin <kcl@thefuturecraft.com>
2025-03-29 15:32:08 +0800

    move suspend

--- app/plus-plus/Counter.tsx
+++ app/plus-plus/Counter.tsx
@@ -1,7 +1,12 @@
 "use client"
 import { useState } from "react"
+import { useSearchParams } from "next/navigation"
 
 const Counter = () => {
+    const searchParams = useSearchParams()
+
+    const user = searchParams.get("user")
+
     const [count, setCount] = useState<number>(0)
 
     const plus = (num: number): void => {
@@ -12,6 +17,7 @@ const Counter = () => {
         <div
             className="flex flex-col"
         >
+            <h1>{user} Plus Plus</h1>
             <span>{count}</span>
             <button
                 onClick={() => plus(1)}
--- app/plus-plus/page.tsx
+++ app/plus-plus/page.tsx
@@ -1,21 +1,14 @@
 "use client"
 import Counter from "./Counter"
-import { useSearchParams } from "next/navigation"
 import { Suspense } from 'react'
 
 const PlusPlus = () => {
-    const searchParams = useSearchParams()
-
-    const user = searchParams.get("user")
-
-
 
     return(
         <main
             className="flex flex-col"
         >
             <Suspense fallback={<SuspendFallback/>}>
-                <h1>{user} Plus Plus</h1>
                 <Counter />
             </Suspense>
             
